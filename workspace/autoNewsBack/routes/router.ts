01ef967d9d1c0fe9116b9de0160f59e67fa8c70f
Leandro C. Tomassini <leandroctomasini@gmail.com>
2023-09-09 14:26:23 -0300

    tn

--- workspace/autoNewsBack/routes/router.ts
+++ workspace/autoNewsBack/routes/router.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from 'express';
+import puppeteer from "puppeteer";
 
 import Server from '../classes/server';
 
@@ -8,12 +9,44 @@ import { usuariosConectados } from '../sockets/socket';
 const router = Router();
 
 
+router.get('/tn', async (req: Request, resp: Response) => {
+    const browser = await puppeteer.launch({
+        headless: false,
+        executablePath: "C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe",
+    });
+
+    const page = await browser.newPage();
+
+    await page.goto("https://tn.com.ar/ultimas-noticias/pagina/1/");
+
+    const newsLinks = await page.evaluate(() => {
+        const noticiasTarjetas = document.querySelectorAll('.card__container');
+
+        const nuevasNoticias = Array.from(noticiasTarjetas).map((tarjetaNoticia) => {
+            const enlace = tarjetaNoticia.querySelector('a.card__image.card__media');
+            const href = enlace ? enlace.getAttribute('href') : null;
+
+            return {
+                linkNoticia: 'https://tn.com.ar' + href,
+            };
+        });
+
+        return nuevasNoticias;
+    });
+
+    resp.json(newsLinks);
+
+    await browser.close();
+});
+
 router.get('/mensajes', (req: Request, resp: Response) => {
+
     resp.status(200).json({
         ok: true,
         mensaje: 'Todo está bien !!!'
     });
 });
+
 
 router.post('/mensajes', (req: Request, resp: Response) => {
 
