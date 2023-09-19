6a2c468292de0dcf213fb3278d57ca8131d3378a
Leandro C. Tomassini <leandroctomassini@gmail.com>
2023-09-19 17:24:15 -0300

    detalles

--- workspace/autoNewsFront/js/script.js
+++ workspace/autoNewsFront/js/script.js
@@ -3,6 +3,9 @@ let voices = [];
 let voiceSelect = document.querySelector("select");
 let noticias = [];
 let currentNoticiaIndex = 0;
+let currentImageIndex = 0;
+const imageSliderInner = document.querySelector(".image-slider-inner");
+const reproducirBtn = document.querySelector("button");
 
 window.speechSynthesis.onvoiceschanged = () => {
   voices = window.speechSynthesis.getVoices();
@@ -15,10 +18,11 @@ voiceSelect.addEventListener("change", () => {
   speech.voice = voices[voiceSelect.value];
 });
 
-document.querySelector("button").addEventListener("click", () => {
-  console.log('BOTON TOCADO')
+reproducirBtn.addEventListener("click", () => {
+  console.log('BOTON TOCADO');
   noticias = [];
   currentNoticiaIndex = 0;
+  currentImageIndex = 0;
 
   fetch('http://localhost:5000/tn')
     .then(response => {
@@ -28,18 +32,14 @@ document.querySelector("button").addEventListener("click", () => {
       return response.json();
     })
     .then(data => {
-
       if (Array.isArray(data)) {
-
         data.forEach(noticia => {
           let noticiaCompleta = "";
-
           noticiaCompleta += `${noticia.h1}. `;
           noticiaCompleta += `${noticia.h2}. `;
 
           if (noticia.parrafos != undefined) {
             noticia.parrafos.forEach((parrafo, index) => {
-
               if (!parrafo.startsWith("Leé también:")) {
                 const parrafoSinParentesis = parrafo.replace(/\([^)]*\)/g, '');
                 noticiaCompleta += `${parrafoSinParentesis}. `;
@@ -47,15 +47,12 @@ document.querySelector("button").addEventListener("click", () => {
             });
           }
 
-          // Comprobar si existen la portada y fotos, y si hay al menos 2 fotos
           if (
-            noticia.portada !== undefined &&
             noticia.fotos !== undefined &&
             noticia.fotos.length >= 2
           ) {
             noticias.push({
               contenido: noticiaCompleta.trim(),
-              portada: noticia.portada,
               fotos: noticia.fotos || [],
               h1: noticia.h1,
               h2: noticia.h2
@@ -76,28 +73,45 @@ document.querySelector("button").addEventListener("click", () => {
 function readNextNoticia() {
   if (currentNoticiaIndex < noticias.length) {
     console.log("Comienzo a leer noticia");
-
-    // Mostrar la URL de la portada de la noticia actual
-    console.log("URL de la portada:", noticias[currentNoticiaIndex].portada);
-
-    // Mostrar la URL de cada foto de la noticia actual
     const fotos = noticias[currentNoticiaIndex].fotos || [];
-    fotos.forEach((foto, index) => {
-      console.log(`URL de la foto ${index + 1}:`, foto);
-    });
-
-    // Mostrar h1 y h2 de la noticia actual
+    console.log(`Número de fotos: ${fotos.length}`);
     console.log("h1:", noticias[currentNoticiaIndex].h1);
     console.log("h2:", noticias[currentNoticiaIndex].h2);
 
+    const imageElement = document.querySelector(".image-slider-inner img");
+
     speech.text = noticias[currentNoticiaIndex].contenido;
+    window.speechSynthesis.speak(speech);
+
+    function changeImage() {
+      if (currentImageIndex < fotos.length) {
+        imageElement.src = fotos[currentImageIndex];
+        currentImageIndex++;
+      } else {
+        currentImageIndex = 0; // Volver al principio cuando se han mostrado todas las imágenes
+      }
+
+      if (!speech.paused) {
+        // Si el discurso no está en pausa, continuar cambiando las imágenes
+        setTimeout(changeImage, 3000);
+      }
+    }
+
+    speech.onstart = () => {
+      console.log("Comienzo a leer texto");
+      changeImage(); // Comenzar a cambiar las imágenes cuando comienza el discurso
+    };
+
     speech.onend = () => {
       console.log("Terminé de leer noticia");
       currentNoticiaIndex++;
       if (currentNoticiaIndex < noticias.length) {
         readNextNoticia();
+      } else {
+        // Si se han leído todas las noticias, volver al principio
+        currentNoticiaIndex = 0;
+        readNextNoticia();
       }
     };
-    window.speechSynthesis.speak(speech);
   }
 }
--- workspace/autoNewsFront/js/slide.js
+++
@@ -1,9 +0,0 @@
-// Este es solo un ejemplo de cómo puedes iniciar la animación, ajusta según tus necesidades
-const imageSliderInner = document.querySelector(".image-slider-inner");
-
-function startAnimation() {
-    imageSliderInner.style.animationPlayState = "running";
-}
-
-// Iniciar la animación después de que la página se haya cargado completamente
-window.addEventListener("load", startAnimation);
