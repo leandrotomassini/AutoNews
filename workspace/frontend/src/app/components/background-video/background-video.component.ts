d1bc01fbee8729d69d8fe94597f613b2a734566a
Leandro C. Tomassini <leandroctomassini@gmail.com>
2023-09-21 15:00:08 -0300

    videos background

--- workspace/frontend/src/app/components/background-video/background-video.component.ts
+++ workspace/frontend/src/app/components/background-video/background-video.component.ts
@@ -6,34 +6,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./background-video.component.css'],
 })
 export class BackgroundVideoComponent implements OnInit {
+
+  videoSources = [
+    '../../assets/videos/1.mp4',
+    '../../assets/videos/2.mp4',
+    '../../assets/videos/4.mp4',
+    '../../assets/videos/5.mp4',
+    '../../assets/videos/6.mp4',
+    '../../assets/videos/7.mp4',
+    '../../assets/videos/8.mp4',
+  ];
+
+  currentVideoIndex = 0;
+
   ngOnInit(): void {
     this.rotateBackgroundVideos();
   }
 
   private rotateBackgroundVideos() {
-    const videoElements = [
-      document.getElementById('background-video1') as HTMLVideoElement,
-      document.getElementById('background-video2') as HTMLVideoElement,
-      document.getElementById('background-video3') as HTMLVideoElement,
-    ];
-    const videoSources = [
-      'assets/videos/1.mp4',
-      'assets/videos/2.mp4',
-      'assets/videos/3.mp4',
-    ];
-    let currentIndex = 0;
+    const videoElement = document.getElementById(
+      'background-video'
+    ) as HTMLVideoElement;
 
-    videoElements.forEach((videoElement) => {
-      videoElement.muted = true;
-    });
+    const changeRandomVideo = () => {
+      // Escoge un nuevo índice de video aleatorio
+      const randomVideoIndex = Math.floor(
+        Math.random() * this.videoSources.length
+      );
 
-    setInterval(() => {
-      currentIndex = (currentIndex + 1) % videoSources.length;
-      videoElements.forEach((videoElement, index) => {
-        videoElement.src = videoSources[index];
-        videoElement.load();
-        videoElement.play();
-      });
-    }, 30000);
+      // Cambia el video actual
+      this.currentVideoIndex = randomVideoIndex;
+
+      // Carga y reproduce el nuevo video seleccionado
+      videoElement.src = this.videoSources[randomVideoIndex];
+      videoElement.load();
+      videoElement.play();
+    };
+
+    // Cambiar aleatoriamente cada tres segundos (3000 milisegundos)
+    setInterval(changeRandomVideo, 20000);
+
+    // Iniciar la reproducción del primer video al cargar la página
+    videoElement.src = this.videoSources[this.currentVideoIndex];
+    videoElement.load();
+    videoElement.play();
+  }
+
+  get currentVideoSource(): string {
+    return this.videoSources[this.currentVideoIndex];
   }
 }
