1b88a47a3a97a31fdda32d21028a78806fa7efa0
Leandro C. Tomassini <leandroctomassini@gmail.com>
2023-09-22 15:09:18 -0300

    graph

--- workspace/autoNewsFront/README.md
+++
@@ -1,3 +0,0 @@
-![Text to Speech](https://i.imgur.com/6C6lJuI.png)
-# Text to Speech
-Program created in javascript to translate text to speech. Developed by Leandro C. Tomassini
--- workspace/frontend/src/app/pages/main-screen/main-screen.component.ts
+++ workspace/frontend/src/app/pages/main-screen/main-screen.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NewsService } from '../../services/news.service';
-import { NewsDatum, NewsListResponse } from 'src/app/interface';
+import { NewsDatum } from 'src/app/interface';
 
 @Component({
   selector: 'app-main-screen',
   templateUrl: './main-screen.component.html',
   styleUrls: ['./main-screen.component.css'],
 })
-export class MainScreenComponent implements OnInit {
+export class MainScreenComponent implements OnInit, OnDestroy {
   speech = new SpeechSynthesisUtterance();
   voices: SpeechSynthesisVoice[] = [];
   newsList: NewsDatum[] = [];
@@ -16,26 +16,46 @@ export class MainScreenComponent implements OnInit {
   isSpeaking = false;
   title: string = '';
   titles: string[] = [];
+  private newsSubscription: any;
 
   constructor(private newsService: NewsService) {}
 
   ngOnInit() {
+    this.initializeSpeechSynthesis(); // Inicia la síntesis de voz al cargar el componente
     this.getNews();
   }
 
+  ngOnDestroy() {
+    // Asegúrate de cancelar la suscripción cuando el componente se destruye
+    if (this.newsSubscription) {
+      this.newsSubscription.unsubscribe();
+    }
+  }
+
   private getNews() {
-    this.newsService.getNews().subscribe((news) => {
+    // Inicia la solicitud de noticias nuevas de manera asincrónica
+    const newNewsSubscription = this.newsService.getNews().subscribe((news) => {
+      // Limpiar los títulos antiguos y guardar los nuevos títulos
+      this.titles = news.newsData.map((newData) => newData.h1);
+      // Una vez que lleguen las noticias nuevas, reemplaza las noticias actuales
       this.newsList = news.newsData;
-      this.titles = this.newsList.map((newData) => newData.h1);
-      this.initializeSpeechSynthesis();
+      // Comienza a leer las noticias cuando llegan
+      this.speakNext();
     });
+
+    // Si ya había una solicitud en curso, cancela la suscripción anterior
+    if (this.newsSubscription) {
+      this.newsSubscription.unsubscribe();
+    }
+
+    // Establece la nueva suscripción como la actual
+    this.newsSubscription = newNewsSubscription;
   }
 
   private initializeSpeechSynthesis() {
     if ('speechSynthesis' in window) {
       window.speechSynthesis.onvoiceschanged = () => {
         this.voices = window.speechSynthesis.getVoices();
-        this.speakNext();
       };
     }
   }
@@ -46,8 +66,17 @@ export class MainScreenComponent implements OnInit {
       console.log('Empezando a leer:', newData.h1);
       this.title = newData.h1;
       this.images = newData.images;
-      this.speech.text = newData.combinedText;
+      this.speech.text = newData.h1;
       this.isSpeaking = true;
+
+      // Generar un índice aleatorio entre 0 y 2
+      const randomIndex = Math.floor(Math.random() * 3);
+
+      // Seleccionar el valor del índice aleatorio
+      const voiceIndex = [263, 261, 264][randomIndex];
+      console.log('Estoy usando la voz número: ' + voiceIndex);
+
+      this.speech.voice = this.voices[voiceIndex];
       this.speakText();
 
       this.speech.onend = () => {
@@ -55,14 +84,20 @@ export class MainScreenComponent implements OnInit {
         this.currentIndex++;
         this.isSpeaking = false;
         this.speech.onend = null;
-        this.speakNext();
+
+        // Verificar si se están leyendo las últimas tres noticias
+        if (this.currentIndex >= this.newsList.length - 3) {
+          // Solicitar nuevas noticias mientras se siguen leyendo las actuales
+          this.getNews();
+        } else {
+          // Continuar leyendo las noticias actuales
+          this.speakNext();
+        }
       };
-    } else if (this.currentIndex >= this.newsList.length) {
     }
   }
 
   private speakText() {
-    this.speech.voice = this.voices[263];
     window.speechSynthesis.speak(this.speech);
   }
 }
